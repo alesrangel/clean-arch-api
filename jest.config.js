ff7d8096be75495a7647e09589b5eae06c37f7e3
alesrangel <alesrangel15@gmail.com>
2025-08-30 22:26:25 -0300

    chore: jest config and scripts

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,12 @@
 export default {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts',
+    'src/**/*.{ts,tsx}',
+  '!src/**/*.d.ts',
+  '!src/**/protocols/**', 
+  '!src/**/index.ts',
+  "!src/**/**/**/signup-protocols.ts"
+  ],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   transform: {
